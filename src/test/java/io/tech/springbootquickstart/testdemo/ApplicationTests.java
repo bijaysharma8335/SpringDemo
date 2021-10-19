8aaa9514c0547a0b9de46210165e92b9c2da063d
Bijay Sharma <bijaysharma8335@gmail.com>
2021-10-19 20:50:18 +0530

    springdemo

---
+++ src/test/java/io/tech/springbootquickstart/testdemo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tech.springbootquickstart.testdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
